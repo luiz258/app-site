fa1afcb69668fdad26d851c6ccf61db58877b0cd
LUIZ MARCELO BARBOSA <luizmarcelo258@gmail.com>
2024-02-02 16:54:01 -0300

    improve: add style footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -2,7 +2,7 @@
 export function FooterComponent() {
 
     return (
-        <footer className="bg-white rounded-lg dark:bg-gray-800">
+        <footer className="bg-white dark:bg-gray-800 border-t-2 border-gray-100">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">Black2k</a>. All Rights Reserved.
         </span>
